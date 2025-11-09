4a10c2f85fd7ba7bb8c85c57557dc4d0f00338ab
Danil Sveshnikov <bloodshoww@mail.ru>
2025-11-09 20:41:35 +0300

    back-1 - правки #7

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
